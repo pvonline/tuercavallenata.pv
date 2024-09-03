c0c15163abea7c51dcc0aa18ff8d3fd8f814132a
LUCAS L. PALACIO P. <radioelpalaciovallenato@gmail.com>
2024-09-02 20:30:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+/*_________Inicio Reloj Digital_______*/
+
+const $tiempo = document.querySelector('.tiempo'),
+$fecha = document.querySelector('.fecha');
+
+function digitalClock(){
+    let f = new Date(),
+    dia = f.getDate(),
+    mes = f.getMonth() + 1,
+    anio = f.getFullYear(),
+    diaSemana = f.getDay();
+
+    dia = ('0' + dia).slice(-2);
+    mes = ('0' + mes).slice(-2)
+
+    let timeString = f.toLocaleTimeString();
+    $tiempo.innerHTML = timeString;
+
+    let semana = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
+    let showSemana = (semana[diaSemana]);
+    $fecha.innerHTML = `${showSemana}  ${dia}-${mes}-${anio}`
+}
+setInterval(() => {
+    digitalClock()
+}, 1000);
+
+/*______Final Reloj Digital________*/
+
+
+
+const swiper = new Swiper('.slider-wrapper', {
+
+    loop: true,
+    grabCursor:true,
+    spaceBetween:30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView:1
+        },
+        768: {
+            slidesPerView:2
+        },
+        1024: {
+            slidesPerView:3
+        },
+    }
+  });
